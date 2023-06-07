3574eb8a5f5e3cba88a7b2133f54033bdd421d6e
Rick Shiguemoto <shiguemoto88@gmail.com>
2023-06-06 22:26:55 -0300

    FPS movement and camera movement, made a simple map too

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,46 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float playerSpeed;
+    [SerializeField] private float gravity;
+    [SerializeField] private float jumpForce;
+
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckRadius;
+    [SerializeField] private LayerMask groundMask;
+
+    [SerializeField] private CharacterController playerCharacterController;
+    [SerializeField] private BoxCollider playerBoxCollider;
+
+    //This is only used for Y velocity...
+    Vector3 velocity;
+    
+    private bool isGrounded;
 
     void Update()
     {
-        
+        isGrounded = Physics.CheckBox(playerBoxCollider.bounds.center, 0.5f * playerBoxCollider.bounds.size, Quaternion.identity, groundMask);
+
+        // Checking if the player can jump
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = 0f;
+            if (Input.GetButtonDown("Jump"))
+            {
+                // Jump!
+                velocity.y += jumpForce;
+            }
+        }
+
+        // Calculating and Applying gravity
+        velocity.y += gravity * Time.deltaTime;
+        playerCharacterController.Move(velocity * Time.deltaTime);
+
+        // Movement at the direction we're pointing at
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        playerCharacterController.Move(move * playerSpeed * Time.deltaTime);
     }
 }
