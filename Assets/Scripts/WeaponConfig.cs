8aa4b10ee0d7acfbb56e10fb90c5ac125040a21d
Rick Shiguemoto <shiguemoto88@gmail.com>
2023-08-23 20:50:47 -0300

    Finished prototyping weapons and zombies

--- Assets/Scripts/WeaponConfig.cs
+++ Assets/Scripts/WeaponConfig.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WeaponConfig : MonoBehaviour {
-    public int currentBulletCount;
+    [HideInInspector] public int currentBulletCount;
     public int maxBulletCount;
     public int damage;
     public bool isAutomatic;
